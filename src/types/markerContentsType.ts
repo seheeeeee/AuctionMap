77f43af5fde0755f915d47010e5a5e12f5e67d8c
seheeeeee <euc728@naver.com>
2023-07-10 14:34:54 +0900

    marker contents type 정의, top type contents 설정

---
+++ src/types/markerContentsType.ts
@@ -0,0 +1,14 @@
+// 마커에 표시될 데이터
+// type - top: 시/도, mid: 시/군/구, bot: 동/읍/면, item: 매물마커, low: 다중 매물마커, multi: 매물-실거래 둘다, real: 실거래마커, realDetail: 실거래 큰마커
+interface MarkerContentsType {
+    type: "top" | "mid" | "bot" | "item" | "low" | "multi" | "real" | "realDetail";
+    displayName: string;
+    count: number;
+    item?: any;
+    isSearch?: boolean;
+    real: null | any;
+    markerType: "product" | "real";
+    lng: number;
+    lat: number;
+}
+export type { MarkerContentsType };
