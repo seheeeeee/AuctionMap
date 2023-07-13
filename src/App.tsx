7444b4d0bbae24bf48a19b88dd2e81fbfdc05a48
seheeeeee <euc728@naver.com>
2023-07-13 22:45:24 +0900

    package.json env 스크립트 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import "./styles/global.css";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
-    const url = new URL(process.env.PUBLIC_URL || "http://localhost");
-
     useEffect(() => {
         let vh = window.innerHeight * 0.01;
         document.documentElement.style.setProperty("--vh", `${vh}px`);
